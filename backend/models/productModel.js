2409757f6cf4c60c6c2d64ad3f69e1c343b655d9
SamKlep <shamdemann23@gmail.com>
2021-02-14 15:38:07 -0600

    productModel, orderModel, userModel & added config to gitignore.;

---
+++ backend/models/productModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
