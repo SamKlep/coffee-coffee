73f516e3c989a6e8610d0cf4a246a16e097c935e
SamKlep <shamdemann23@gmail.com>
2021-02-14 19:06:27 -0600

    orderRoutes, productRoutes & userRoutes.

--- backend/data/products.js
+++ backend/data/products.js
@@ -3,7 +3,6 @@ const products = [
     name: 'Coffee',
     type: 'Coffee',
     price: 3,
-
     description:
       'Darkly colored, bitter, slightly acidic and has a stimulating effect in humans, primarily due to its caffeine content. It is one of the most popular drinks in the world, and can be prepared and presented in a variety of ways (e.g., espresso, French press, caffè latte, or already-brewed canned coffee).',
     image: '/images/coffee.jpg',
@@ -12,7 +11,6 @@ const products = [
     name: 'Espresso',
     type: 'Espresso',
     price: 5,
-
     description:
       'Full-flavored, concentrated form of coffee that is served in “shots.” It is made by forcing pressurized hot water through very finely ground coffee beans using an espresso machine. ... The crema adds to the rich flavor and lingering aftertaste of espresso',
     image: '/images/espresso.jpg',
@@ -21,7 +19,6 @@ const products = [
     name: 'Cappuccino',
     type: 'Espresso',
     price: 5,
-
     description:
       'Coffee drink that today is typically composed of a single espresso shot and hot milk, with the surface topped with foamed milk. Cappuccinos are most often prepared with an espresso machine.',
     image: '/images/cappuccino.jpg',
@@ -30,9 +27,10 @@ const products = [
     name: 'Cafe Late',
     type: 'Latte',
     price: 5,
-
     description:
       'A latte is a milky espresso drink you can order at most American cafes. Many lattes have delicate patterns or pictures drawn in the creamy foam on top. The full name of a latte is "caffe latte," which means "milk coffee" in Italian.',
     image: '/images/latte.jpg',
   },
 ]
+
+export default products
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import path from 'path'
 
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+
 dotenv.config()
 
 connectDB()
@@ -17,6 +21,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json())
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.get('/', (req, res) => {
   res.send('API is running....')
